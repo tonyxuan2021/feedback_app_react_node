74f488142c35b05bdacf3a8f12cead1a947984d4
tonyxuan2021 <tonyyexuan@gmail.com>
2022-10-17 13:24:53 -0700

    populate home page with tickets from provided json file

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import FeedbackTicket from "../components/FeedbackTicket";
 import HeaderFeedback from "../components/HeaderFeedback";
 import HeaderSort from "../components/HeaderSort";
+import data from "../data/data.json";
 
 const HomePage = () => {
+  const [feedbackData, setFeedbackData] = useState(data);
+
   return (
     <div>
       <HeaderFeedback />
       <HeaderSort />
-      <FeedbackTicket />
+      <FeedbackTicket feedbackData={feedbackData} />
     </div>
   );
 };
